c6a011142fb1f4c65d5b102ef0f6f18208ab094d
agrawaldisha71 <121039698+agrawaldisha71@users.noreply.github.com>
2022-12-19 20:03:09 +0530

    Add files via upload

---
+++ Numberguessing.java
@@ -0,0 +1,33 @@
+package Numberguessing;
+import java.lang.Math;
+import java.util.*;
+class NumberGuessing
+{
+public static void main(String args[])
+{
+ int guess=1,user,random;
+ random = (int)(Math.random()*100);
+ Scanner sc = new Scanner(System.in);
+ System.out.println("Enter number between 0 to 100");
+ user = sc.nextInt();
+ while(user!= random)
+ {
+ if(user>random)
+ {
+  System.out.println("Number is greater..");
+  System.out.println("Try Again!!");
+  user = sc.nextInt();
+  guess++;
+ }
+ 
+ if(user<random)
+ {
+  System.out.println("Number is Small..");
+  System.out.println("Try Again!!");
+  user = sc.nextInt();
+  guess++;
+  }
+ }
+ System.out.println("**Congratulations You Won by "+guess+" guesses**");
+}
+}
